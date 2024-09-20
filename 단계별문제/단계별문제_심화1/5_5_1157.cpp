0db81236aaec90f533d8f6650224d60fd83fe564
작별의 작업공간 <97824848+ZakByeol@users.noreply.github.com>
2024-09-20 21:09:02 +0900

    09/20(금) 심화

---
+++ 단계별문제/단계별문제_심화1/5_5_1157.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <cstring>
+// 아직 덜 품
+bool GetTrue(char Input[]){
+
+
+    return true;
+}
+
+int main(){
+    char Input_value[10],Storage_value[10];
+    int count[100];
+    scanf("%s", Input_value);
+    Storage_value[0] = Input_value[0];
+
+    // 문자열을 돌려보면서, 이전 알파벳과 다르면, 해당 알파벳을 저장한 뒤, 카운트
+    for (int i = 0; i < strlen(Input_value) - 1; ++i) {
+
+    }
+
+    return 0;
+}
