15e29373ecf1921a2acf416e8a8ba4ab62290847
Baegeonwoo <97824848+Baegeonwoo@users.noreply.github.com>
2025-06-09 21:35:14 +0900

    2025/06/08 Max Head Commit

--- [Solved]Class3/11279_MaxHeap/main.cpp
+++ [Solved]Class3/11279_MaxHeap/main.cpp
@@ -1,81 +1,95 @@
-#include<stdio.h>
-#define MAX 100001
+#include <iostream>
+#pragma warning(disable: 4996)
 
-typedef struct _Heap {
-	int numOfData;
-	int arr[MAX];
-} Heap;
+int minhArray[100001];
 
-void initHeap(Heap* heap);
-void Insert(Heap* heap, int data);
-int Delete(Heap* heap);
-
-int main() {
-	int CaseAmount, CalculateType, i;
-	Heap heap;
-	initHeap(&heap);
-	scanf("%d", &CaseAmount);
-	for (i = 0; i < CaseAmount; i++) {
-		scanf("%d", &CalculateType);
-		if (CalculateType == 0) {
-			if (!heap.numOfData)
-				printf("0\n");
-			else
-				printf("%d\n", Delete(&heap));
-		}
-		else {
-			Insert(&heap, CalculateType);
-		}
-	}
-
-	return 0;
+void SwapElement(int* _pArray1, int* _pArray2) // Change Element by using pointer
+{
+	int tmp = *_pArray1;
+	*_pArray1 = *_pArray2;
+	*_pArray2 = tmp;
 }
 
-void initHeap(Heap* heap) {
-	heap->numOfData = 0;
-}
+int main()
+{
+	int vCalculate; // case amount
+	int vInput;
+	int vAmount = 0; // inference array element amount
+	int vNowIndex; // locate now index
 
-void Insert(Heap* _Heap, int _InsertData) {
-	int idx = _Heap->numOfData + 1;
-	while (idx != 1) {
-		if (_InsertData > _Heap->arr[idx / 2]) {
-			_Heap->arr[idx] = _Heap->arr[idx / 2];
-			idx /= 2;
-		}
-		else
-			break;
-	}
-	_Heap->arr[idx] = _InsertData;
-	_Heap->numOfData++;
-}
+	scanf("%d", &vCalculate);
 
-int Delete(Heap* _Heap) {
-	int root = _Heap->arr[1];
-	int last = _Heap->arr[_Heap->numOfData];
-	_Heap->numOfData--;
+	for (int i = 0; i < vCalculate; i++)
+	{
+		scanf("%d", &vInput);
+		if (vInput > 0) // MinHeap Add element
+		{
+			vAmount++;
+			vNowIndex = vAmount - 1;
+			minhArray[vNowIndex] = vInput;
+			while (true)
+			{
+				if (vNowIndex == 0) // exception process
+				{
+					break;
+				}
 
-	int pIdx = 1, Lchild = pIdx * 2, Rchild = pIdx * 2 + 1;
-	int cIdx;
-	while (Lchild <= _Heap->numOfData) {
-		if (Lchild == _Heap->numOfData) {
-			if (last < _Heap->arr[Lchild]) {
-				cIdx = Lchild;
-				_Heap->arr[pIdx] = _Heap->arr[cIdx];
-				pIdx = cIdx;
+				if (minhArray[vNowIndex] >= minhArray[(vNowIndex - 1) / 2]) // compare and change
+				{
+					SwapElement(minhArray + vNowIndex, minhArray + ((vNowIndex - 1) / 2));
+					vNowIndex = (vNowIndex - 1) / 2;
+				}
+				else
+				{
+					break;
+				}
 			}
-			break;
 		}
-		else {
-			cIdx = _Heap->arr[Lchild] > _Heap->arr[Rchild] ? Lchild : Rchild;
-			if (last > _Heap->arr[cIdx])
-				break;
-			else
-				_Heap->arr[pIdx] = _Heap->arr[cIdx];
+		else // MinHeap Delete Element
+		{
+			if (vAmount == 0) // initial case
+			{
+				printf("%d\n", minhArray[0]);
+				continue;
+			}
+
+			// printf and remove process
+			printf("%d\n", minhArray[0]);
+			minhArray[0] = minhArray[vAmount - 1];
+			minhArray[vAmount - 1] = 0;
+			vAmount--;
+			vNowIndex = 0;
+
+			while (true)
+			{
+				int leftChild = 2 * vNowIndex + 1;
+				int rightChild = 2 * vNowIndex + 2;
+				int SwapIndex = vNowIndex;
+				bool doSwap = false;
+
+				// check smaller index
+				if (leftChild < vAmount && minhArray[leftChild] > minhArray[SwapIndex])
+				{
+					SwapIndex = leftChild;
+					doSwap = true;
+				}
+				if (rightChild < vAmount && minhArray[rightChild] > minhArray[SwapIndex]) // 왼쪽보다 오른쪽이 더 작아짐!
+				{
+					SwapIndex = rightChild;
+					doSwap = true;
+				}
+
+				if (doSwap) // Swap according to doSwap value
+				{
+					SwapElement(minhArray + vNowIndex, minhArray + SwapIndex);
+					vNowIndex = SwapIndex;
+				}
+				else
+				{
+					break;
+				}
+			}
 		}
-		pIdx = cIdx;
-		Lchild = pIdx * 2;
-		Rchild = pIdx * 2 + 1;
 	}
-	_Heap->arr[pIdx] = last;
-	return root;
+	return 0;
 }
